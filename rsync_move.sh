f51ac6a50c2fdc2208d217e004241c01540fc651
minkolazer <minkolazer@users.noreply.github.com>
2018-04-12 20:57:54 +0300

    Use global timeout

--- rsync_move.sh
+++ rsync_move.sh
@@ -2,9 +2,8 @@
 
 set -eo pipefail
 
-FLOCK_TIMEOUT=${FLOCK_TIMEOUT:-700}
-RSYNC_TIMEOUT=${RSYNC_TIMEOUT:-600}
-LOCK_FILE=/tmp/rsync.lock
+TIMEOUT=${TIMEOUT:-600}
+LOCK_FILE=${LOCK_FILE:-/tmp/rsync.lock}
 
 SRC_DIR=/home/user/to_send
 SRC_MASK='*.mp3'
@@ -26,11 +25,11 @@ main() {
 check() {
   if [ ! $(find ${SRC_DIR} -maxdepth 0 -type d -empty 2>/dev/null) ]; then
     [[ "$run" == "true" ]] && log 'new files are created during rsync, restarting...'
-    return 0  # true
+    return 1
   fi
 
   [[ "$run" == "false" ]] && log 'no new files are found'
-
+  
   exit 0
 }
 
@@ -39,8 +38,8 @@ sync() {
 
   run=true
 
-  flock -n --timeout ${FLOCK_TIMEOUT} ${LOCK_FILE} -c \
-    "rsync -chvt --include ${SRC_MASK} --timeout=${RSYNC_TIMEOUT} --remove-source-files ${SRC_DIR}/* ${DST_SRV}:${DST_DIR}"
+  flock -n ${LOCK_FILE} -c \
+    "timeout ${TIMEOUT} rsync -chvt --include ${SRC_MASK} --remove-source-files ${SRC_DIR}/* ${DST_SRV}:${DST_DIR}"
 
   log 'success!'
 }
@@ -50,4 +49,3 @@ log() {
 }
 
 main "$@"
-
